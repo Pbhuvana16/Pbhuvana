e7b910fcce8d280873180aae730c52ba5d7d57d2
Pbhuvana16 <143376015+Pbhuvana16@users.noreply.github.com>
2023-09-20 14:55:16 +0530

    Update getsubjects.py

--- getrestapi/getsubjects.py
+++ getrestapi/getsubjects.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
+import json
 
 # Create a Flask web application instance
 app = Flask(__name__)
+
+# Read the config.json file
+with open('config.json', 'r') as config_file:
+    config_data = json.load(config_file)
 
 # Define a route that responds to HTTP GET requests at '/'
 @app.route('/', methods=['GET'])
 def get():
     # Return a JSON format of subjects
-    return jsonify(subjects)
+    return jsonify(config_data['subjects'])
 
 # Define a route that responds to HTTP GET requests at '/<marks>'
 @app.route('/<marks>', methods=['GET'])
@@ -15,7 +20,7 @@ def get_subjects(marks):
     subjects_in_same_marks = []
 
     # Iterate through the subjects list
-    for item in subjects:
+    for item in config_data['subjects']:
         if item['marks'] == marks:
             subjects_in_same_marks.append(item)
 
