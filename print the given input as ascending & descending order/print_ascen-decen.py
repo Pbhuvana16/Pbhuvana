5561d161c69f1398225dacf2df93f06e1a27b7e5
Pbhuvana16 <143376015+Pbhuvana16@users.noreply.github.com>
2023-09-21 06:45:00 +0530

    source code file

--- print the given input as ascending & descending order/print_ascen-decen.py
+++ print the given input as ascending & descending order/print_ascen-decen.py
@@ -1,31 +1,45 @@
-numbers = []
-words = []
+# Function to input numbers
+def input_numbers():
+    numbers = []
+    while True:
+        num = input("Enter a number (or 0 to stop): ")
+        if num == '0':
+            break
+        try:
+            numbers.append(int(num))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    return numbers
 
-# Input loop for numbers
-while True:
-    num = input("Enter a number (or 0 to stop): ")
-    if num == '0':
-        break
-    try:
-        numbers.append(int(num))
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
+# Function to input words
+def input_words():
+    words = []
+    while True:
+        word = input("Enter a word (or END to stop): ")
+        if word == 'END':
+            break
+        words.append(word)
+    return words
 
-# Input loop for words
-while True:
-    word = input("Enter a word (or END to stop): ")
-    if word == 'END':
-        break
-    words.append(word)
+# Function to print numbers in ascending and descending order
+def print_sorted_numbers(numbers):
+    numbers.sort()
+    print("Numbers in ascending order:", numbers)
+    numbers.sort(reverse=True)
+    print("Numbers in descending order:", numbers)
 
-# Sort and print numbers
-numbers.sort()
-print("Numbers in ascending order:", numbers)
-numbers.sort(reverse=True)
-print("Numbers in descending order:", numbers)
+# Function to print words in ascending and descending order
+def print_sorted_words(words):
+    words.sort()
+    print("Words in ascending order:", words)
+    words.sort(reverse=True)
+    print("Words in descending order:", words)
 
-# Sort and print words
-words.sort()
-print("Words in ascending order:", words)
-words.sort(reverse=True)
-print("Words in descending order:", words)
+if __name__ == "__main__":
+    # Call input functions to get numbers and words
+    numbers = input_numbers()
+    words = input_words()
+
+    # Call print functions to display sorted numbers and words
+    print_sorted_numbers(numbers)
+    print_sorted_words(words)
