5315f969ec9f9ca77c80cacb08798ae8ce11a906
Pbhuvana16 <143376015+Pbhuvana16@users.noreply.github.com>
2023-11-09 15:39:16 +0530

    main.tf

---
+++ Exercise 36 /terraform script to create a Ubuntu EC2 VM and run an ansible script to print a message /main.tf
@@ -0,0 +1,34 @@
+terraform {
+ required_providers {
+  aws = {
+   source = "hashicorp/aws"
+   version = "~> 5.0"
+  }
+ }
+}
+provider "aws" {
+ region   = "ap-southeast-2"
+ access_key = "AKIAYPAWHJC4ZFYW3A53"
+ secret_key = "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"
+}
+resource "tls_private_key" "rsa_4096" {
+ algorithm = "RSA"
+ rsa_bits = 4096
+}
+variable "key_name" {}
+resource "aws_key_pair" "key_pair" {
+ key_name  = var.key_name
+ public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+resource "local_file" "private_key" {
+ content = tls_private_key.rsa_4096.private_key_pem
+ filename = var.key_name
+}
+resource "aws_instance" "public_instance" {
+ ami      = "ami-0df4b2961410d4cff"
+ instance_type = "t2.micro"
+ key_name   = aws_key_pair.key_pair.key_name
+ tags = {
+  Name = "public_instance"
+ }
+}
